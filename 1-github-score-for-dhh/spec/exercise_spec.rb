bd425cd11168323a12cb491b173a50d2e8fdf721
Lance Ennen <byennen@gmail.com>
2017-04-09 16:16:33 -0400

    first challenge is done, moving on

--- 1-github-score-for-dhh/spec/exercise_spec.rb
+++ 1-github-score-for-dhh/spec/exercise_spec.rb
@@ -2,24 +2,26 @@
 require 'spec_helper'
 
 describe Exercise::Calculator do
-  before do
-    @score = Exercise::Calculator.new
-    @url = 'https://api.github.com/users/dhh/events/public'
-  end
+  context "with dhh github url" do
+    before do
+      @score = Exercise::Calculator.new
+      @url = 'https://api.github.com/users/dhh/events/public'
+    end
 
-  it 'Should initialize the Calculator class' do
-    expect(@score.is_a?(Exercise::Calculator)).to eq true
-    expect(@score.url).to eq @url
-    expect(@score.commit_type.is_a?(Hash)).to eq true
-  end
+    it 'initializes the Calculator' do
+      expect(@score.is_a?(Exercise::Calculator)).to eq true
+      expect(@score.url).to eq @url
+      expect(@score.commit_type.is_a?(Hash)).to eq true
+    end
 
-  it 'Should calculate the score' do
-    expect { @score.calculate_score }.to change { @score.score }.by_at_least(1)
-    expect(@score.calculate_score.is_a?(Integer)).to eq true
-  end
+    it 'calculates the score' do
+      expect {@score.calculate_score}.to change {@score.score}.by_at_least(1)
+      expect(@score.calculate_score.is_a?(Integer)).to eq true
+    end
 
-  it 'Should run the program' do
-    output = @score.run
-    expect(output.include?("DHH's github score is")).to eq true
+    it 'runs the program' do
+      output = @score.run
+      expect(output.include?("DHH's github score is")).to eq true
+    end
   end
 end
