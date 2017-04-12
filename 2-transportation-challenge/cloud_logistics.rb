e672eaee340040d98a7ead24dedd62b31d66609c
Lance Ennen <byennen@gmail.com>
2017-04-11 20:37:20 -0400

    challenge 2 complete for now, code review still needed, test output?

--- 2-transportation-challenge/cloud_logistics.rb
+++ 2-transportation-challenge/cloud_logistics.rb
@@ -1,3 +1,83 @@
-module CloudLogistics
+require 'active_record'
+require 'sqlite3'
 
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+ActiveRecord::Schema.define do
+  create_table :feights do |table|
+    table.column :carrier, :string
+    table.column :origin, :string
+    table.column :destination, :string
+  end
+  create_table :stops do |table|
+    table.column :feight_id, :integer
+    table.column :address, :string
+  end
 end
+
+module CloudLogistics
+  class Stop < ActiveRecord::Base
+    belongs_to :feights
+  end
+
+  class Feight < ActiveRecord::Base
+    has_many :stops
+
+    def load_questions
+      @feight = Feight.new
+      @stops = number_of_stops
+      @feight.carrier = question_string('Carrier name:')
+      @feight.origin = question_string('Origin:')
+      ask_for_stops(@feight, @stops)
+      @feight.destination = question_string('Destination:')
+      @feight.save
+    end
+
+    def run
+      load_questions
+      puts '---'
+      puts "Bill of lading number #{@feight.stops.count}, being carried by #{@feight.carrier}"
+      puts "- Origin: #{@feight.origin}"
+      @feight.stops.each do |stop|
+        puts "- #{stop.id} stop: #{stop.address}"
+      end
+      puts "- Destination: #{@feight.destination}"
+    end
+
+    private
+
+    def number_of_stops
+      puts 'Number of stops:'
+      while (input = gets.chomp.to_i) <= 0
+        puts 'Please enter a valid number'
+      end
+      input
+    end
+
+    def ask_for_stops(feight, stops)
+      stops.times do |stop|
+        puts "Stop number #{stop + 1}:"
+        while (input = gets.chomp.to_s) && input.empty? || input.nil? ||
+              input.match(/^[-+]?[0-9]*$/)
+          puts 'Please enter a valid answer'
+        end
+        feight.stops.new(address: input)
+      end
+    end
+
+    def question_string(question)
+      puts question
+      while (input = gets.chomp) && input.empty? ||
+            input.nil? || input.match(/^[-+]?[0-9]*$/)
+        puts 'Please enter a valid answer'
+      end
+      input
+    end
+  end
+end
+
+@feight = CloudLogistics::Feight.new
+puts @feight.run
--- 2-transportation-challenge/spec/cloud_logistics_spec.rb
+++ 2-transportation-challenge/spec/cloud_logistics_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
 describe CloudLogistics do
+  describe CloudLogistics::Stop do
+    before do
+      @stop = CloudLogistics::Stop.new
+      @stop.address = 'West Palm Beach'
+      @stop.save
+    end
 
+    it 'has an address' do
+      expect(@stop.address).to eq('West Palm Beach')
+    end
+  end
+
+  describe CloudLogistics::Feight do
+    before do
+      @feight = CloudLogistics::Feight.new
+      @feight.stops.new(address: 'Orlando')
+      @feight.stops.new(address: 'Chicago')
+      @feight.stops.new(address: 'New York')
+      @feight.carrier = 'YRC'
+      @feight.origin = 'Ft Lauderdale'
+      @feight.destination = 'Orlando'
+      @feight.save
+    end
+
+    it 'has multiple stops' do
+      expect(@feight.stops.count).to eq(3)
+    end
+
+    it 'has a carrier' do
+      expect(@feight.carrier).to eq('YRC')
+    end
+
+    it 'has a origin' do
+      expect(@feight.origin).to eq('Ft Lauderdale')
+    end
+
+    it 'has a destination' do
+      expect(@feight.destination).to eq('Orlando')
+    end
+
+  end
 end
